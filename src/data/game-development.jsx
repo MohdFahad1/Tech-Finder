31c52c14f7f81f8382f1522fd0a67367e826b502
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-12-14 11:37:52 +0530

    feat: Added data in mobile development

--- src/data/game-development.jsx
+++ src/data/game-development.jsx
@@ -4,20 +4,44 @@ export const categoryData = {
   name: "Game Development",
   description:
     "Crafting interactive games for various platforms, including consoles, PCs, and mobile.",
-  icon: <Gamepad2 className="w-6 h-6  text-gray-900" />,
+  icon: <Gamepad2 className="w-6 h-6 text-gray-900" />,
   path: "/browse/game-development",
   subCategories: [
     {
       name: "Languages",
       path: "languages",
+      items: [
+        {
+          name: "C#",
+          slug: "csharp",
+          url: "https://learn.microsoft.com/en-us/dotnet/csharp/",
+          // icon: CSharpIcon,
+        },
+      ],
     },
     {
       name: "Engines",
       path: "engines",
-    },
-    {
-      name: "Middleware",
-      path: "middleware",
+      items: [
+        {
+          name: "Unity",
+          slug: "unity",
+          url: "https://unity.com/",
+          // icon: UnityIcon,
+        },
+        {
+          name: "Unreal Engine",
+          slug: "unreal-engine",
+          url: "https://unrealengine.com/",
+          // icon: UnrealEngineIcon,
+        },
+        {
+          name: "Godot",
+          slug: "godot",
+          url: "https://godotengine.org/",
+          // icon: GodotIcon,
+        },
+      ],
     },
   ],
 };
