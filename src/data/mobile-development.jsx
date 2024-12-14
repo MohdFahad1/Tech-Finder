60dc87a16c43d11f6beb53415f23bc0eb11ff6fe
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-12-14 11:35:38 +0530

    feat: Added data in mobile development

--- src/data/mobile-development.jsx
+++ src/data/mobile-development.jsx
@@ -3,16 +3,42 @@ import { Smartphone } from "lucide-react";
 export const categoryData = {
   name: "Mobile Development",
   description: "Creating apps for mobile devices like smartphones and tablets.",
-  icon: <Smartphone className="w-6 h-6  text-gray-900" />,
+  icon: <Smartphone className="w-6 h-6 text-gray-900" />,
   path: "/browse/mobile",
   subCategories: [
     {
       name: "Frontnend",
       path: "frontend",
-    },
-    {
-      name: "Payment",
-      path: "payments",
+      item: {
+        Languages: [
+          {
+            name: "Swift",
+            slug: "swift",
+            url: "https://developer.apple.com/swift/",
+            // icon: SwiftIcon,
+          },
+          {
+            name: "Kotlin",
+            slug: "kotlin",
+            url: "https://kotlinlang.org/",
+            // icon: KotlinIcon,
+          },
+          {
+            name: "Dart",
+            slug: "dart",
+            url: "https://dart.dev/",
+            // icon: DartIcon,
+          },
+        ],
+        Frameworks: [
+          {
+            name: "SwiftUI",
+            slug: "swiftui",
+            url: "https://developer.apple.com/documentation/swiftui",
+            // icon: AppleIcon,
+          },
+        ],
+      },
     },
   ],
 };
