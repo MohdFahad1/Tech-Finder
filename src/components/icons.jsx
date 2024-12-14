78410ad68e2f656fc162c14276d37d885e4d69f0
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-12-14 13:30:03 +0530

    feat: Added icons

---
+++ src/components/icons.jsx
@@ -0,0 +1,196 @@
+// icons.tsx
+
+import { DrizzleIcon } from "@/components//icons/drizzle";
+
+import { RecoilIcon } from "@/components//icons/recoil";
+import { ReduxIcon } from "@/components//icons/redux";
+import { ZustandIcon } from "@/components//icons/zustand";
+import { AceternityUIIcon } from "@/components/icons/aceternity-ui";
+import { AngularIcon } from "@/components/icons/angular";
+import { AntDesignIcon } from "@/components/icons/ant-design";
+import { AppleIcon } from "@/components/icons/apple";
+import { AppwriteIcon } from "@/components/icons/appwrite";
+import { AstroIcon } from "@/components/icons/astro";
+import { Auth0Icon } from "@/components/icons/auth0";
+import { ChakraIcon } from "@/components/icons/chakra";
+import { ClerkIcon } from "@/components/icons/clerk";
+import { CloudinaryIcon } from "@/components/icons/cloudinary";
+import { ConvexIcon } from "@/components/icons/convex";
+import { CreateExpoStackIcon } from "@/components/icons/create-expo-stack";
+import { CSharpIcon } from "@/components/icons/csharp";
+import { CssIcon } from "@/components/icons/css";
+import { DasiyUIIcon } from "@/components/icons/daisy-ui";
+import { DartIcon } from "@/components/icons/dart";
+import { DjangoIcon } from "@/components/icons/django";
+import { ElectronIcon } from "@/components/icons/electron";
+import { ExpoIcon } from "@/components/icons/expo";
+import { FirebaseIcon } from "@/components/icons/firebase";
+import { FlaskIcon } from "@/components/icons/flask";
+import { FlutterIcon } from "@/components/icons/flutter";
+import { GoIcon } from "@/components/icons/go";
+import { GodotIcon } from "@/components/icons/godot";
+import { GoogleColabIcon } from "@/components/icons/google-colab";
+import { HerokuIcon } from "@/components/icons/heroku";
+import { HtmlIcon } from "@/components/icons/html";
+import { HtmxIcon } from "@/components/icons/htmx";
+import { HuggingFaceIcon } from "@/components/icons/hugging-face";
+import { IconGeneratorAIIcon } from "@/components/icons/icon-generator-ai";
+import { JavaIcon } from "@/components/icons/java";
+import { JavaScriptIcon } from "@/components/icons/javascript";
+import { JestIcon } from "@/components/icons/jest";
+import { JqueryIcon } from "@/components/icons/jquery";
+import { JupyterIcon } from "@/components/icons/jupyter";
+import { KaggleIcon } from "@/components/icons/kaggle";
+import { KerasIcon } from "@/components/icons/keras";
+import { KindeIcon } from "@/components/icons/kinde";
+import { KotlinIcon } from "@/components/icons/kotlin";
+import { LaravelIcon } from "@/components/icons/laravel";
+import { LuciaIcon } from "@/components/icons/lucia";
+import { MaterialUIIcon } from "@/components/icons/material-ui";
+import { MicrosoftIcon } from "@/components/icons/microsoft";
+import { MlflowIcon } from "@/components/icons/mlflow";
+import { MongoDBIcon } from "@/components/icons/mongodb";
+import { NeonIcon } from "@/components/icons/neon";
+import { NestJsIcon } from "@/components/icons/nestjs";
+import { NetlifyIcon } from "@/components/icons/netlify";
+import { NextUIIcon } from "@/components/icons/next-ui";
+import { NextJsIcon } from "@/components/icons/nextjs";
+import { NuxtIcon } from "@/components/icons/nuxt";
+import { OpenCVIcon } from "@/components/icons/opencv";
+import { PhoenixFrameworkIcon } from "@/components/icons/phoenix-framework";
+import { PhpIcon } from "@/components/icons/php";
+import { PlaywrightIcon } from "@/components/icons/playwright";
+import { PrismaIcon } from "@/components/icons/prisma";
+import { PythonIcon } from "@/components/icons/python";
+import { PyTorchIcon } from "@/components/icons/pytouch";
+import { RadixIcon } from "@/components/icons/radix";
+import { RailwayIcon } from "@/components/icons/railway";
+import { ReactIcon } from "@/components/icons/react";
+import { ReactNativeIcon } from "@/components/icons/react-native";
+import { ReactNativeReanimatedIcon } from "@/components/icons/react-native-reanimated";
+import { RemixIcon } from "@/components/icons/remix";
+import { RenderIcon } from "@/components/icons/render";
+import { RevenueCatIcon } from "@/components/icons/revenue-cat";
+import { RubyIcon } from "@/components/icons/ruby";
+import { RubyOnRailsIcon } from "@/components/icons/ruby-on-rails";
+import { ShadcnIcon } from "@/components/icons/shadcn";
+import { ShoutoutIcon } from "@/components/icons/shoutout";
+import { SolidJSIcon } from "@/components/icons/solidjs";
+import { StripeIcon } from "@/components/icons/stripe";
+import { StyledComponentsIcon } from "@/components/icons/styledcomponents";
+import { SupabaseIcon } from "@/components/icons/supabase";
+import { SvelteIcon } from "@/components/icons/svelte";
+import { SwiftIcon } from "@/components/icons/swift";
+import { SyntaxUIIcon } from "@/components/icons/syntax-ui";
+import { TailwindCSSIcon } from "@/components/icons/tailwindcss";
+import { TensorFlowIcon } from "@/components/icons/tensorflow";
+import { TestCafeIcon } from "@/components/icons/testcafe";
+import { TypeORMIcon } from "@/components/icons/typeorm";
+import { TypeScriptIcon } from "@/components/icons/typescript";
+import { UnityIcon } from "@/components/icons/unity";
+import { UnrealEngineIcon } from "@/components/icons/unreal-engines";
+import { UploadThingIcon } from "@/components/icons/uploadthing";
+import { UpstashIcon } from "@/components/icons/upstash";
+import { VercelIcon } from "@/components/icons/vercel";
+import { VueIcon } from "@/components/icons/vue";
+import { WatermelonDBIcon } from "@/components/icons/watermelon-db";
+import { ZodIcon } from "@/components/icons/zod";
+
+export {
+  SyntaxUIIcon,
+  CannyIcon,
+  ShoutoutIcon,
+  MlflowIcon,
+  PhoenixFrameworkIcon,
+  TestCafeIcon,
+  WatermelonDBIcon,
+  ReactNativeReanimatedIcon,
+  ReduxIcon,
+  ZustandIcon,
+  RecoilIcon,
+  MaterialUIIcon,
+  ChakraIcon,
+  AntDesignIcon,
+  AceternityUIIcon,
+  DasiyUIIcon,
+  NextUIIcon,
+  GodotIcon,
+  UnrealEngineIcon,
+  MicrosoftIcon,
+  CSharpIcon,
+  GoogleColabIcon,
+  JupyterIcon,
+  PyTorchIcon,
+  KerasIcon,
+  OpenCVIcon,
+  TensorFlowIcon,
+  HuggingFaceIcon,
+  KaggleIcon,
+  AppleIcon,
+  RevenueCatIcon,
+  ShadcnIcon,
+  RadixIcon,
+  RenderIcon,
+  AppwriteIcon,
+  SolidJSIcon,
+  ZodIcon,
+  TypeORMIcon,
+  LuciaIcon,
+  HerokuIcon,
+  DartIcon,
+  CreateExpoStackIcon,
+  AgilityIcon,
+  AngularIcon,
+  Auth0Icon,
+  ClerkIcon,
+  FirebaseIcon,
+  JestIcon,
+  KindeIcon,
+  MongoDBIcon,
+  NeonIcon,
+  NestJsIcon,
+  NetlifyIcon,
+  NextJsIcon,
+  NuxtIcon,
+  PlaywrightIcon,
+  RailwayIcon,
+  RemixIcon,
+  StripeIcon,
+  StyledComponentsIcon,
+  SupabaseIcon,
+  TailwindCSSIcon,
+  VercelIcon,
+  SvelteIcon,
+  ReactIcon,
+  VueIcon,
+  HtmxIcon,
+  AstroIcon,
+  ConvexIcon,
+  IconGeneratorAIIcon,
+  LaravelIcon,
+  PrismaIcon,
+  HtmlIcon,
+  CssIcon,
+  JavaScriptIcon,
+  TypeScriptIcon,
+  JqueryIcon,
+  PhpIcon,
+  JavaIcon,
+  RubyIcon,
+  GoIcon,
+  RubyOnRailsIcon,
+  DjangoIcon,
+  PythonIcon,
+  FlaskIcon,
+  DrizzleIcon,
+  UpstashIcon,
+  CloudinaryIcon,
+  UploadThingIcon,
+  ElectronIcon,
+  UnityIcon,
+  ExpoIcon,
+  ReactNativeIcon,
+  KotlinIcon,
+  SwiftIcon,
+  FlutterIcon,
+};
