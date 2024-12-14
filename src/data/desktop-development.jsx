3e9421bfd84bfc504586dc6824ee54b06150bda2
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-12-14 11:39:15 +0530

    feat: Added data in desktop development

--- src/data/desktop-development.jsx
+++ src/data/desktop-development.jsx
@@ -4,16 +4,38 @@ export const categoryData = {
   name: "Desktop Development",
   description:
     "Designing and implementing software that runs natively on desktop operating systems.",
-  icon: <Monitor className="w-6 h-6  text-gray-900" />,
+  icon: <Monitor className="w-6 h-6 text-gray-900" />,
   path: "/browse/desktop-development",
   subCategories: [
     {
       name: "Languages",
       path: "languages",
+      items: [
+        {
+          name: "C#",
+          slug: "csharp",
+          url: "https://learn.microsoft.com/en-us/dotnet/csharp/",
+          // icon: CSharpIcon,
+        },
+      ],
     },
     {
       name: "Frameworks",
       path: "frameworks",
+      items: [
+        {
+          name: "Electron",
+          slug: "electron",
+          url: "https://electronjs.org/",
+          // icon: ElectronIcon,
+        },
+        {
+          name: "Flutter",
+          slug: "flutter",
+          url: "https://flutter.dev/",
+          // icon: FlutterIcon,
+        },
+      ],
     },
   ],
 };
