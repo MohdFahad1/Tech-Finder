97caa13ecc5365e7f52e2fb8c4bc5f1381a45805
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-12-14 11:41:33 +0530

    feat: Added cross platform development data

--- src/data/cross-platform-development.jsx
+++ src/data/cross-platform-development.jsx
@@ -4,20 +4,66 @@ export const categoryData = {
   name: "Cross Platform Development",
   description:
     "Developing applications that work across multiple platforms with one codebase.",
-  icon: <MonitorSmartphone className="w-6 h-6  text-gray-900" />,
+  icon: <MonitorSmartphone className="w-6 h-6 text-gray-900" />,
   path: "/browse/cross-platform",
   subCategories: [
     {
       name: "Frontend",
       path: "frontend",
+      items: {
+        Frameworks: [
+          {
+            name: "React Native",
+            slug: "react-native",
+            url: "https://reactnative.dev/",
+            // icon: ReactNativeIcon,
+          },
+          {
+            name: "Flutter",
+            slug: "flutter",
+            url: "https://flutter.dev/",
+            // icon: FlutterIcon,
+          },
+          {
+            name: "Electron",
+            slug: "electron",
+            url: "https://electronjs.org/",
+            // icon: ElectronIcon,
+          },
+        ],
+      },
     },
     {
       name: "Tooling",
       path: "tooling",
+      items: [
+        {
+          name: "Expo",
+          slug: "expo",
+          url: "https://expo.dev/",
+          // icon: ExpoIcon,
+        },
+        {
+          name: "Create Expo Stack",
+          slug: "create-expo-stack",
+          url: "https://createexpostack.com/",
+          // icon: CreateExpoStackIcon,
+        },
+      ],
     },
     {
       name: "Databases",
       path: "databases",
+      items: {
+        Frameworks: [
+          {
+            name: "WatermelonDB",
+            slug: "watermelondb",
+            url: "https://watermelondb.dev/docs",
+            // icon: WatermelonDBIcon,
+          },
+        ],
+      },
     },
   ],
 };
