8da138c2e44002cb62c3b767f0c71e8694f940cb
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-12-19 12:55:32 +0530

    chore: Removed unused packages & code

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
 import Categories from "@/components/categories";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
