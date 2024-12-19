aaf3cdb46f3ed3ba5e5e9ffe0daa6a42614cdacc
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-12-19 21:12:45 +0530

    feat: Added animation in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,30 @@
+"use client";
+
 import Categories from "@/components/categories";
 import Hero from "@/components/Hero";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function Home() {
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.from(".first_container, .second_container", {
+      y: 10,
+      opacity: 0,
+      duration: 0.3,
+      delay: 0.2,
+      stagger: 0.18,
+    });
+  });
   return (
     <div>
-      <Hero />
-      <Categories />
+      <div className="first_container">
+        <Hero />
+      </div>
+      <div className="second_container">
+        <Categories />
+      </div>
     </div>
   );
 }
